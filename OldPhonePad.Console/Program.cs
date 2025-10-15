4faa75cbab24db30895e908df22bf9d863afee8c
vubon <vubon.roy@gmail.com>
2025-10-16 00:35:48 +0700

    Add solution codebase

---
+++ OldPhonePad.Console/Program.cs
@@ -0,0 +1,37 @@
+﻿using OldPhonePad;
+
+namespace OldPhonePad.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("------ Old Phone Pad -------");
+            System.Console.WriteLine("Enter key sequences ending with # (or `quit` to exit)");
+            System.Console.WriteLine();
+
+            while (true)
+            {
+                System.Console.Write("Enter number: ");
+                var input = System.Console.ReadLine();
+                if (string.IsNullOrEmpty(input) || string.Equals(input, "quit"))
+                {
+                    break;
+                }
+
+                try
+                {
+                    var result = OldPhonePad.Process(input);
+                    System.Console.WriteLine("Output: " + result);
+                    System.Console.WriteLine("------------------");
+                }
+                catch (Exception e)
+                {
+                    System.Console.WriteLine("Unknown error: " + e.Message);
+                    throw;
+                }
+            }
+            System.Console.WriteLine("Bye Bye!");
+        }
+    }
+};
---
+++ OldPhonePad/OldPhonePad.cs
@@ -0,0 +1,105 @@
+﻿using System.Text;
+
+namespace OldPhonePad
+{
+    public static class OldPhonePad
+    {
+        private static readonly Dictionary<char, string> KeyMapping = new Dictionary<char, string>()
+        {
+            { '2', "ABC" },
+            { '3', "DEF" },
+            { '4', "GHI" },
+            { '5', "JKL" },
+            { '6', "MNO" },
+            { '7', "PQRS" },
+            { '8', "TUV" },
+            { '9', "WXYZ" },
+        };
+
+        private static char GetLetterFromKey(char key, int pressCount)
+        {
+            if (!KeyMapping.ContainsKey(key))
+                return '\0';
+            var letters = KeyMapping[key];
+            var index = (pressCount - 1) % letters.Length;
+            return letters[index];
+        }
+
+        public static string Process(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                throw new ArgumentException("Input cannot be empty or null", nameof(input));
+            if (!input.EndsWith("#"))
+                throw new ArgumentException("Input must end with '#'", nameof(input));
+            
+            var result = new StringBuilder();
+            var currentKey = '\0';
+            var pressCount = 0;
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                var currentChar = input[i];
+                if (currentChar == '#')
+                {
+                    if (currentKey != '\0')
+                    {
+                        result.Append(GetLetterFromKey(currentKey, pressCount));
+                    }
+                    break;
+                }
+
+                if (currentChar == '*')
+                {
+                    if (currentKey != '\0')
+                    {
+                        result.Append(GetLetterFromKey(currentKey, pressCount));
+                    }
+
+                    if (result.Length > 0)
+                    {
+                        result.Length--; // Remove last character
+                    }
+                    // Reset current key state 
+                    currentKey = '\0';
+                    pressCount = 0;
+                    continue;
+                }
+                
+                // Handle space (pause) early return 
+                if (currentChar == ' ')
+                {
+                    if (currentKey != '\0')
+                    {
+                        result.Append(GetLetterFromKey(currentKey, pressCount));
+                    }
+                    // Reset for next key 
+                    currentKey = '\0';
+                    pressCount = 0;
+                    continue;
+                }
+                // Skip non-number keys 
+                if (!KeyMapping.ContainsKey(currentChar)) 
+                    continue;
+                
+                // Handle number keys - main logic without nesting 
+                if (currentKey == currentChar)
+                {
+                    // Same key pressed again
+                    pressCount++;
+                }
+                else
+                {
+                    if (currentKey != '\0')
+                    {
+                        result.Append(GetLetterFromKey(currentKey, pressCount));
+                    }
+                    // start new key 
+                    currentKey = currentChar; // Track which key
+                    pressCount = 1; // Press Once 
+                }
+            }
+            
+            return result.ToString();
+        }
+    }
+}
